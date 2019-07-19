fa9ecf21cd8e54b1f01cd2314ae21ac3310eb3b0
zengzhiqiang <749970817@qq.com>
2019-07-19 21:25:41 +0800

    编写送检单的保存程序
    1、将送检单需要填写的信息以列表的形式传递给save_me()函数。
    2、保存送检单信息以后将当前送检单编号存入配置文件。

--- commissions_of_test/admin.py
+++ commissions_of_test/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Commission
 
 # Register your models here.
+
+admin.site.register(Commission)
--- commissions_of_test/models.py
+++ commissions_of_test/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-import datetime, os
+import datetime
+import os
 
 # Create your models here.
 
@@ -9,19 +10,36 @@ class Commission(models.Model):
     creat_time = models.DateTimeField(auto_now_add=True)
 
     def create_commission_id(self):
-        '''自动生成送检单编号（导入的历史送检单不可调用该函数）'''
+        '''
+        1、自动生成送检单编号（导入的历史送检单不可调用该函数）
+        2、在送检单最后保存时需要将送检单编号写入配置文件
+        '''
         if self.commission_id == "":
             try:
                 filepath = os.path.join(os.path.abspath("."), "oaklims\\配置文件.txt")
-                fr = open(filepath, "rw")
+                fr = open(filepath, "r")
             except FileNotFoundError as reason:
                 print("无法打开配置文件，请联系管理员！！\n%s" % str(reason))
             else:
                 words = fr.readline().split("-")
-                if words[0] != str(datetime.datetime.now().year) or words[1] != str(datetime.datetime.now().month):
+                if words[0] != str(datetime.datetime.now().year) or words[1] != str(datetime.datetime.now().month).zfill(2):
                     self.commission_id = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month).zfill(2) + "-" + "1".zfill(3)
                 else:
-                    self.commission_id = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month).zfill(2) + "-" + str(int(words[3])+1).zfill(3)
-                pass   #将配置写入文件需要增加
+                    self.commission_id = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().month).zfill(2) + "-" + str(int(words[2])+1).zfill(3)
+                fr.close()
         else:
             pass
+
+    def save_me(self, commission_info):
+        '''
+        save之前需要
+        1、将送检编号存入配置文件。
+        :param product:
+        :return: None
+        '''
+        self.product_id = commission_info[0]
+        self.save()
+        filepath = os.path.join(os.path.abspath("."), "oaklims\\配置文件.txt")
+        with open(filepath, "w") as fr:    #将当前送检单编号写入配置文件
+            fr.write(self.commission_id)
+
