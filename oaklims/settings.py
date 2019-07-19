2382f2532d7ba7a7f2da5ce60f7b97e2b82699e8
zengzhiqiang <749970817@qq.com>
2019-07-19 23:20:08 +0800

    将product加入insideapp

--- oaklims/settings.py
+++ oaklims/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'commissions_of_test'
+    'commissions_of_test',
+    'product'
 ]
 
 MIDDLEWARE = [
