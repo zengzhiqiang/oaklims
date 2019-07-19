555a85ca8fa65bdc4b8416c2e4885583b22360ff
zengzhiqiang <749970817@qq.com>
2019-07-19 22:48:34 +0800

    创建product的简易模型
    创建product的简易模型

--- product/models.py
+++ product/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Product(models.Model):
+    '''产品的模型'''
+    part_no = models.CharField(max_length=128)
+    part_no_customer = models.CharField(max_length=128)
+    project_name = models.CharField(max_length=128)
+    product_specifications = models.CharField(max_length=128)
