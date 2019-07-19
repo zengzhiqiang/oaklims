684e784d69863c096f9ceff8a41384e406b811fe
zengzhiqiang <749970817@qq.com>
2019-07-19 23:11:37 +0800

    创建径向疲劳测试模型
    创建径向疲劳测试模型
    创建样品模型（内容带填充）

--- wheel_radial_fatigue_test/models.py
+++ wheel_radial_fatigue_test/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from commissions_of_test.models import Commission
 
 # Create your models here.
+
+class WhRaFaTest(models.Model):
+    '''
+    径向送检单的模型
+    '''
+    #wh_ra_fa_test_id = models.CharField(max_length=128)    待定是否需要该字段
+    commission_id = models.ForeignKey(Commission, on_delete=True)
+    reference_standard = models.CharField(max_length=128)   #所引用的标准，外键与标准关联
+    number_of_samples = models.IntegerField()
+    special_requirement = models.TextField()
+    note = models.TextField()
