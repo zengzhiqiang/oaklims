c7c6f0ef3dc51f16c1ae678d33165b77aaca26b5
zengzhiqiang <749970817@qq.com>
2019-07-23 17:50:54 +0800

    样品中关联所采用的标准

--- wheel_radial_fatigue_test/models.py
+++ wheel_radial_fatigue_test/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from commissions_of_test.models import Commission
+from .method.detail_of_stand import WhRaFaTeStandDetail
 
 # Create your models here.
 
@@ -9,8 +10,7 @@ class WhRaFaTest(models.Model):
     '''
     #wh_ra_fa_test_id = models.CharField(max_length=128)    待定是否需要该字段
     commission_id = models.ForeignKey(Commission, on_delete=True)
-    reference_standard = models.CharField(max_length=128)   #所引用的标准，外键与标准关联
-    standard_more = models.CharField(max_length=128)     #与所引用标准的细节相关联，载荷系数。
+    standard_more = models.ForeignKey(WhRaFaTeStandDetail, on_delete=True)   #与所引用标准的细节相关联，载荷系数。
     number_of_samples = models.IntegerField()
     special_requirement = models.TextField()
     note = models.TextField()
