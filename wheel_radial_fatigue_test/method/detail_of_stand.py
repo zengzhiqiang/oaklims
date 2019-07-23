b2539b961fde89d0ac86635cc974b0c239e6a553
zengzhiqiang <749970817@qq.com>
2019-07-23 17:45:44 +0800

    给方法里面的两个模型添加了基本的字段

--- wheel_radial_fatigue_test/method/detail_of_stand.py
+++ wheel_radial_fatigue_test/method/detail_of_stand.py
@@ -1,1 +1,15 @@
 from django.db import models
+from .list_of_stand import WhRaFaTeStand
+
+class WhRaFaTeStandDetail(models.Model):
+    CRACK_ALLOW_CHOICE = (
+        (True, "允许"),
+        (False, "不允许"),
+    )
+    wh_ra_fa_stand_id = models.ForeignKey(WhRaFaTeStand, on_delete=True)
+    test_factor = models.FloatField()
+    requirement = models.BigIntegerField()
+    allow_crack = models.BooleanField(choices=CRACK_ALLOW_CHOICE)
+    stand_detail_description = models.TextField()
+    stand_detail_note = models.TextField()
+
--- wheel_radial_fatigue_test/method/list_of_stand.py
+++ wheel_radial_fatigue_test/method/list_of_stand.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class WhRaFaTeStand(models.Model):
+    stand_code = models.CharField(max_length=128)   #标准号
+    stand_name = models.CharField(max_length=128)   #标准名称
+    stand_tag = models.CharField(max_length=128)    #标签(可以是现行有效的标签或者其他)
+    stand_note = models.TextField()                 #备注
