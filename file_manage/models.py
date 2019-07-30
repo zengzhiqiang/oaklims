60514da51505bb6da271e619e06dd1b7226ce30c
zengzhiqiang <749970817@qq.com>
2019-07-30 22:05:11 +0800

    完善了报告上传app
    1、报告按报告编号的年份，月份存储
    2、报告名自动填写函数

--- file_manage/models.py
+++ file_manage/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import os
+from django.conf import settings
+
 
 # Create your models here.
+
+def report_path(instance, filename):
+    '''根据文件名返回存储路径'''
+    return filename[0:4] + '//' + filename[5:7] + '//' + filename
+
+
+class ReportManage(models.Model):
+    '''报告文件管理app'''
+    file_name = models.CharField(max_length=32)
+    file = models.FileField(upload_to=report_path)
+
+    def __str__(self):
+        return self.file_name
+
+    class Meta:
+        verbose_name = "报告列表"
+        verbose_name_plural = "报告列表"
+
+    def get_file_name(self):
+        self.file_name = self.file.name
+
--- oaklims/settings.py
+++ oaklims/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'product',
     'wheel_radial_fatigue_test',
     'apparatus',
+    'file_manage',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'media'
